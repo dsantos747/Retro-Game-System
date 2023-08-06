60a92756d938b9b3f8eff8d88bf057768217d327
dsantos747 <d.santos74747@gmail.com>
2023-08-06 12:39:34 +0100

    .gitignore commited

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 # Main Menu for game selection
 
+# Add "logout" button for username
+# Add link to SQL table for username logging
+
 import pygame
 import Snake
 
@@ -20,6 +23,9 @@ red = '#ff0000'
 green = '#00ff00'
 blue = '#0000ff'
 darkGreen = '#007010'
+lightGrey = '#f9f9f9'
+medLightGrey = '#bbbbbb'
+meddarkGrey = '#707070'
 
 dis_width=400
 dis_height=300
@@ -32,6 +38,7 @@ fps = 60
 clock = pygame.time.Clock()
 
 menu_font = pygame.font.SysFont('bahnschrift', 18)
+form_font = pygame.font.SysFont('Arial', 15)
 
 objects = []
 
@@ -85,6 +92,51 @@ class Button():
         ])
         dis.blit(self.buttonSurface, self.buttonRect)
 
+class formBox():
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.colour = meddarkGrey
+        self.textcolour = black
+        self.text = text
+        self.txt_surface = form_font.render(text, True, self.colour)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+                self.text = ''
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.colour = medLightGrey if self.active else meddarkGrey
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    # HERE NEED TO ADD FUNCTIONALITY TO LINK TO SQL
+                    self.text = 'Logged in as ' + self.text
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = form_font.render(self.text, True, self.textcolour)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, ((self.rect.x)+(self.rect.w/2)-(self.txt_surface.get_width()/2), (self.rect.y)+(self.rect.h/2)-(self.txt_surface.get_height()/2)))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.colour, self.rect, 2)
+
 def launch_snake():
     Snake.main()
 
@@ -93,10 +145,15 @@ def launch_space():
 
 button_width=200
 button_height=40
+field_width=200
+field_height=30
 
 snake_button = Button((dis_width/2)-(button_width/2), 100, button_width, button_height, 'Slippery Snek', launch_snake)
 space_button = Button((dis_width/2)-(button_width/2), 150, button_width, button_height, 'Space Invaders', launch_space)
 
+username_field = formBox((dis_width/2)-(field_width/2),250, field_width, field_height, 'Enter Username for Hi-Scores')
+
+form_boxes = [username_field]
 
 def menuLoop():
     menu_close=False
@@ -107,7 +164,17 @@ def menuLoop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 menu_close=True
+            for box in form_boxes:
+                box.handle_event(event)
         
+        for box in form_boxes:
+            box.update()
+           # box.draw(dis)
+
+        dis.fill(white)
+        for box in form_boxes:
+            box.draw(dis)
+
         for object in objects:
             object.process()
 
